ced688c4834658c66901e52ffad0b395acd8585c
Esteban Viera <esteban.viera.arroba@gmail.com>
2023-05-21 20:16:15 -0300

    style: change main font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,16 @@ import { useApollo } from "@/lib/apolloClient";
 import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
-import { Inter, Roboto } from "next/font/google";
+import { Roboto } from "next/font/google";
 
-const inter = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
+const roboto = Roboto({ weight: "400", subsets: ["latin-ext"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo({});
 
   return (
     <ApolloProvider client={apolloClient}>
-      <main className={inter.className}>
+      <main className={roboto.className}>
         <Component {...pageProps} />
       </main>
     </ApolloProvider>
