b925e855728cd088ecc3bd5a33fcffcd29f3c49d
Esteban Viera <esteban.viera.arroba@gmail.com>
2023-05-22 20:08:21 -0300

    fix: not show pagination if there's only a page

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -14,6 +14,8 @@ const Pagination: React.FC<PaginationProps> = ({
   const pagesCount = Math.ceil(total / perPage);
   const pages = Array.from({ length: pagesCount }, (_, i) => i + 1);
 
+  if (pages.length === 1) return (<></>);
+
   return (
     <div className="flex justify-center mt-4">
       {/* TODO: Add ellipsis pagination */}
@@ -22,9 +24,8 @@ const Pagination: React.FC<PaginationProps> = ({
         <button
           key={page}
           onClick={() => onPageChange(page)}
-          className={`mx-1 px-3 py-2 border rounded-md ${
-            currentPage === page ? "bg-yellow-300 " : " hover:bg-yellow-100"
-          }`}
+          className={`mx-1 px-3 py-2 border rounded-md ${currentPage === page ? "bg-yellow-300 " : " hover:bg-yellow-100"
+            }`}
         >
           {page}
         </button>
