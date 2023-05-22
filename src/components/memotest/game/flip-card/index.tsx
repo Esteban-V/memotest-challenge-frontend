f71f27f74a6e09e586f026428f500a1ff2cf8438
Esteban Viera <esteban.viera.arroba@gmail.com>
2023-05-22 17:51:08 -0300

    refactor: flip-card component

--- src/components/memotest/game/flip-card/index.tsx
+++ src/components/memotest/game/flip-card/index.tsx
@@ -1,26 +1,24 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
 import { FlipCardProps } from "./types";
 
-function FlipCard({ backImage, frontText, isFlipped, onClick }: FlipCardProps) {
+const FlipCard: React.FC<FlipCardProps> = ({ backImage, frontText, isFlipped, onClick }) => {
   return (
     <div
-      onClick={onClick}
-      className={`relative w-36 h-64 cursor-pointer perspective`}
+      onClick={!isFlipped ? onClick : undefined}
+      className="relative w-36 h-64 cursor-pointer perspective"
     >
       <div
-        className={`relative preserve-3d w-full h-full duration-700 ease-in-out ${
-          isFlipped ? "rotate-y-180" : ""
-        }`}
+        className={`relative preserve-3d w-full h-full duration-700 ease-in-out ${isFlipped ? "rotate-y-180" : ""}`}
       >
         <div
-          className={`absolute flex items-center justify-center w-full h-full bg-purple-500 hover:bg-purple-600 border-white border-4
-        text-white font-bold text-5xl rounded-2xl shadow-md backface-hidden hover:scale-105 transition-all`}
+          className="absolute flex items-center justify-center w-full h-full bg-purple-500 hover:bg-purple-600 border-white border-4
+        text-white font-bold text-5xl rounded-2xl shadow-md backface-hidden hover:scale-105 transition-all"
         >
           <h1>{frontText}</h1>
         </div>
         <div
-          className={`absolute w-full h-full bg-green-600 rounded-2xl shadow-md rotate-y-180 border-white border-4 backface-hidden overflow-hidden`}
+          className="absolute w-full h-full bg-green-600 rounded-2xl shadow-md rotate-y-180 border-white border-4 backface-hidden overflow-hidden"
         >
           <Image
             width={100}
