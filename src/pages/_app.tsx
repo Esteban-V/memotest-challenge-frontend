6feff31f995a2f5946ddb32819cd674669eada62
Esteban Viera <esteban.viera.arroba@gmail.com>
2023-05-22 02:26:03 -0300

    chore: add redux providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import { useApollo } from "@/lib/apolloClient";
 import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
+import { Provider } from 'react-redux';
 import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
+import { persistor, store } from "@/lib/redux/store";
+import { FLUSH } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react';
 
 const roboto = Roboto({ weight: "400", subsets: ["latin-ext"] });
 
@@ -11,9 +15,13 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <ApolloProvider client={apolloClient}>
-      <main className={roboto.className}>
-        <Component {...pageProps} />
-      </main>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <main className={roboto.className}>
+            <Component {...pageProps} />
+          </main>
+        </PersistGate>
+      </Provider>
     </ApolloProvider>
   );
 }
