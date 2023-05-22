2739eca3e2ce667036a003664e44064a9bc1d05a
Esteban Viera <esteban.viera.arroba@gmail.com>
2023-05-21 23:40:28 -0300

    feat: create flip-card component

---
+++ src/components/memotest/game/flip-card/index.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import { FlipCardProps } from "./types";
+
+function FlipCard({ backImage, frontText }: FlipCardProps) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    if (isFlipped) return;
+
+    setIsFlipped(!isFlipped);
+    setTimeout(() => setIsFlipped(false), 1000);
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      className={`relative m-5 w-36 h-64 cursor-pointer perspective`}
+    >
+      <div
+        className={`relative preserve-3d w-full h-full duration-700 ease-in-out ${
+          isFlipped ? "rotate-y-180" : ""
+        }`}
+      >
+        <div
+          className={`absolute flex items-center justify-center w-full h-full bg-blue-600 border-white border-4
+        text-white font-bold text-5xl rounded-2xl shadow-md backface-hidden hover:scale-105 transition-all`}
+        >
+          <h1>{frontText}</h1>
+        </div>
+        <div
+          className={`absolute w-full h-full bg-green-600 rounded-2xl shadow-md rotate-y-180 border-white border-4 backface-hidden overflow-hidden`}
+        >
+          <Image
+            width={100}
+            height={100}
+            alt={frontText}
+            content="fill"
+            style={{ objectFit: "cover", height: "100%", width: "100%" }}
+            src={backImage}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FlipCard;
---
+++ src/components/memotest/game/flip-card/types.ts
@@ -0,0 +1,4 @@
+export interface FlipCardProps {
+  backImage: string;
+  frontText: string;
+}
