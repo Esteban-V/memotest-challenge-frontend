fd2b88357a8de7c08487fae1659c678ee6108438
Esteban Viera <esteban.viera.arroba@gmail.com>
2023-05-23 01:52:35 -0300

    feat: add memotest creation mutations

--- src/lib/queries/memotest.tsx
+++ src/lib/queries/memotest.tsx
@@ -6,18 +6,33 @@ export type GET_MEMOTESTS_PAGINATED_TYPE = {
   memoTests: Paginated<MemoTest>;
 };
 
+export type CREATE_MEMOTEST_TYPE = {
+  createMemoTest: Paginated<MemoTest>;
+};
+
 export const GET_MEMOTESTS_PAGINATED = gql`
   query GET_MEMOTESTS_PAGINATED($page: Int!, $perPage: Int!) {
     memoTests(page: $page, first: $perPage) {
       paginatorInfo {
         total
         perPage
+        currentPage
       }
       data {
         id
         name
         image_urls
       }
+    }
+  }
+`;
+
+export const CREATE_MEMOTEST = gql`
+  mutation CREATE_MEMOTEST($name: String!, $images: [String!]!) {
+    createMemoTest(name: $name, image_urls: $images) {
+      id
+      name
+      image_urls
     }
   }
 `;
